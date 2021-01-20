a53ec53cd2da0485a000f9e657c729ba54dfd09a
Kirthan Puttegowda <kirthan.puttegowda@flash.co.za>
2021-01-20 15:23:31 +0200

    initial commit

---
+++ src/test/java/com/example/java/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
